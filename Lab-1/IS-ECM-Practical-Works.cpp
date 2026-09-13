db3f98818079f83ed2728c6dffbd449255e21be1
Павел Павленко <neilnejl451@gmail.com>
2026-09-13 16:17:20 +0300

    Settings

---
+++ Lab-1/IS-ECM-Practical-Works.cpp
@@ -0,0 +1,24 @@
+// IS-ECM-Practical-Works.cpp: определяет точку входа для приложения.
+
+#include "IS-ECM-Practical-Works.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake.5678" << endl;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
