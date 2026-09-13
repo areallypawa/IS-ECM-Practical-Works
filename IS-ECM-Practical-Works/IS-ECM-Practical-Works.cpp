d1abaaae9fd8fbeb0201be9c16998d70f69bb907
Павел Павленко <neilnejl451@gmail.com>
2026-09-13 15:53:33 +0300

    First init lab

---
+++ IS-ECM-Practical-Works/IS-ECM-Practical-Works.cpp
@@ -0,0 +1,12 @@
+﻿// IS-ECM-Practical-Works.cpp: определяет точку входа для приложения.
+//
+
+#include "IS-ECM-Practical-Works.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
